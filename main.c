115d9e5e8ab905607cae6ccfa7b7ebe0e564a805
Josefe17 <josefe17@gmail.com>
2019-03-03 13:31:20 +0100

    Initial commit.

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * TempFanController.c
+ *
+ * Created: 03/03/2019 10:25:22
+ * Author : josefe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
